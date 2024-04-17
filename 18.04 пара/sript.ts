737e04efaeb3728cf7b536cdfeeffd6807711da7
MariiaShat <150503493+MariiaShat@users.noreply.github.com>
2024-04-18 09:49:50 +1000

    new file:   "18.04 \320\277\320\260\321\200\320\260/sript.ts"

---
+++ 18.04 пара/sript.ts
@@ -0,0 +1,113 @@
+abstract class Publisher{ 
+        title: string 
+        author: string
+        pubYear: number
+        copies: number 
+        get getTitle(): string{ 
+            return this.title 
+    } 
+         set setTitle(value:string){ 
+             this.title = value  
+    } 
+    static count: number = 0 
+         constructor(){ 
+         Publisher.count ++ 
+    } 
+    } 
+    class Book extends Publisher implements Reception{ 
+        pages: number 
+        delivery(item: Publisher): void{ 
+     
+        } 
+        receive(item: Publisher): void{ 
+     
+        } 
+    } 
+    class Magazine extends Publisher implements Reception{ 
+        issue: number 
+        delivery(item: Publisher): void{ 
+     
+        } 
+        receive(item: Publisher): void{ 
+             
+        } 
+    } 
+    
+    interface Reception{ 
+        delivery(item: Publisher): void; 
+        receive(item: Publisher): void; 
+    } 
+    
+    class Reader implements Reception{
+        firstName: string
+        lastName: string
+        //
+        static items: Publisher[]
+        //get+set
+        delivery(item:Publisher):void{
+            if(item.copies==0) return
+            if(item.copies == Reader.items.length) return
+            item.copies =  item.copies -1
+        }
+        receive(item:Publisher):void{
+            item.copies=item.copies+1
+        }
+    
+    }
+    class Library{
+        items: Publisher[]
+
+        delivery(item:Publisher):void{
+            this.items.push(item)
+
+        }
+        receive(item:Publisher){
+            this.items.pop()
+        }
+        
+    }
+
+
+
+
+
+
+
+
+
+// abstract class Publish{
+
+
+
+//     static count: number =0
+//     constructor(){
+//     Publisher.count++
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// class Reader{
+//     firstName: string
+//     lastName: string
+
+//     //
+//     items: Publisher[]
+
+//     //get+set
+
+//     delivery(item:Publisher):void{
+//         if(item.copies==0) return
+//         item.copies =  item.copies -1
+//     }
+//     revieve(item:Publisher):void{
+//         item.copies=item.copies+1
+//     }
+
+// }
